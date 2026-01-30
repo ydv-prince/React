371f428d1eb893c5e89781db46770c01b2ce763d
ydv-prince <psychoaimerhere@gmail.com>
2026-01-30 13:55:24 +0530

    Updated to use hooks4

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import HooksExample from './hooks.jsx'
 import HooksExample2 from './hooks2.jsx'
 import HooksExample3 from './hooks3.jsx'
+import HooksExample4 from './hooks4.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <HooksExample />
+      {/* <HooksExample />
       <HooksExample2 />
       <br />
-      <HooksExample3 />
+      <HooksExample3 /> */}
+
+      <HooksExample4/>
     </>
   )
 }
